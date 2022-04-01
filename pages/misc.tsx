89d204f7e3f4e363eab10c82862f81e9eb50618d
Het <het.thakkar809@gmail.com>
2022-03-31 23:11:49 -0400

    update: misc

--- pages/misc.tsx
+++ pages/misc.tsx
@@ -2,14 +2,14 @@ import { IBaseProps } from '../common/interfaces'
 import { NextPage } from 'next'
 import { BookCard } from '../components/BookCard'
 import { Container } from '../components/Container'
-import bookCover from '../public/tales.jpg'
+import bookCover from '../public/children.jpg'
 const Misc: NextPage<IBaseProps> = (props: IBaseProps) => {
   return (
     <Container
       head={{
         title: 'Misc | Het Thakkar',
         description:
-          'Miscellaneous information about Het Thakkar like reading lists, music and playlists. Currently reading Bloodcurdling Tales of Horror and the Macabre',
+          'Miscellaneous information about Het Thakkar like reading lists, music and playlists',
       }}
     >
       <h1 className='content-box text-white font-bold text-4xl mb-10 mt-8'>
@@ -17,12 +17,12 @@ const Misc: NextPage<IBaseProps> = (props: IBaseProps) => {
       </h1>
       <div className='flex flex-col justify-center items-center flex-wrap w-full max-w-2xl mx-auto'>
         <BookCard
-          title='Bloodcurdling Tales of Horror and the Macabre'
-          author='H. P. Lovecraft'
-          description='Regardless of the views of the author, I really enjoyed a few stories I have read of H.P. Lovecraft. Looking forward to reading some great horror stories'
+          title='Children of Dune'
+          author='Frank Herbert'
+          description='Dune Messiah set up a super interesting world, excited to see where this book goes even more excited to see how this gets adapted into a movie.'
           cover={bookCover}
-          coverWidth={300}
-          coverHeight={500}
+          coverWidth={250}
+          coverHeight={380}
           className='mb-8 md:max-h-44'
         />
       </div>
@@ -33,7 +33,7 @@ const Misc: NextPage<IBaseProps> = (props: IBaseProps) => {
         <iframe
           className='w-full'
           height='315'
-          src='https://www.youtube.com/embed/0p0DTgH6GjQ'
+          src='https://www.youtube.com/embed/QM7L27N8-rE'
           title='YouTube video player'
           frameBorder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
@@ -42,11 +42,13 @@ const Misc: NextPage<IBaseProps> = (props: IBaseProps) => {
       </div>
       <div className='content-box text-white mb-4 p-2 md:p-4 rounded-b-lg bg-grayLight flex flex-col'>
         <span className=' font-normal text-2xl'>
-          Romain Garcia - Before You Go EP (Live)
+          Anjunadeep 13 - Mixed by Jody Wisternoff & James Grant (Live from Lake
+          Skadar, Montenegro)
         </span>
         <span className='font-thin text-base mt-2 leading-5'>
-          This EP is just perfect. It&apos;s exactly the kind of music I love to
-          hear and it hits just the right spot
+          What&apos;s better than one of the best deep albums ever to be
+          released? The best deep album played by James Grant and Jody
+          Wisternoff(Who I met recently🤯) at the top of a lake in Montenegro.
         </span>
       </div>
       <div className='content-box text-gray-200 mb-2 mt-10 text-2xl flex flex-col'>
